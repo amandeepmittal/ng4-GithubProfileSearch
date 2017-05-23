e92e093f64c710b25e33e1b58e411258394f3b12
amandeepmittal <amandeepmittal@live.com>
2017-05-23 17:44:21 +0530

    add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { GithubService } from "./github.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

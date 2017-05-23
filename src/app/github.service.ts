37d03121f33d517bec93169bad9231d9cd23720a
amandeepmittal <amandeepmittal@live.com>
2017-05-23 19:33:04 +0530

    fix: from http to https

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -12,7 +12,7 @@ export class GithubService {
   private clientId = '162b6220302bba24b794';
   private clientSecret = 'b2859870711cdeda2419ae6662f378c65aa615c9';
 
-  url = 'http://api.github.com/users/';
+  url = 'https://api.github.com/users/';
 
   // private clientId = '<client_id>';
   // private clientSecret = '<client_secret>';
